f77be10010b6351a27d430b16ecdd64df23fdf26
BSpyral <124008298+BSpyral@users.noreply.github.com>
2024-01-16 14:07:16 -0600

    Add files via upload

--- avers/src/avers/Datos.java
+++ avers/src/avers/Datos.java
@@ -6,14 +6,21 @@ import java.io.FileWriter;
 
 public class Datos {
 
-	String nombre_cuenta,contrasena,telefono,correo;	
-	double saldo;
+	private String nombre_cuenta,telefono,correo;	
+	private double saldo;
 	
-	public Datos(String nombre_cuenta,String contrasena,String telefono) {
+	public Datos(String nombre_cuenta,String telefono) {
 		this.nombre_cuenta=nombre_cuenta;
-		this.contrasena=contrasena;
 		this.telefono=telefono;
 		correo="N/A";
+		saldo=-1;
+	}
+	
+	public Datos() {
+		this.nombre_cuenta="";
+		this.telefono=null;
+		correo="N/A";
+		saldo=-1;
 	}
 		
 	public String getCorreo() {
@@ -28,8 +35,20 @@ public class Datos {
 		return saldo;
 	}
 
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
+	public String getNombre_cuenta() {
+		return nombre_cuenta;
+	}
+
+	public void setNombre_cuenta(String nombre_cuenta) {
+		this.nombre_cuenta = nombre_cuenta;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
 	}
 	////////////////////////////////////////////
 
@@ -52,7 +71,6 @@ public class Datos {
 	private void crear_informacion_usuario() throws IOException {
 		FileWriter fichero=new FileWriter ("Datos.txt");
 		fichero.write(this.nombre_cuenta+"  ");
-		fichero.write(this.contrasena+"  ");
 		fichero.write(this.telefono+"  ");
 		fichero.write(this.correo+"  ");
 		fichero.close();
@@ -63,8 +81,8 @@ public class Datos {
 	////////////////////////////////////////////////////////
 	
 	public static void main(String[] args) throws IOException {
-		Datos Persona=new Datos("Bar","mipit18","7711922383");
-		Datos Persona2=new Datos("hola","1234","prueba");
+		Datos Persona=new Datos("Bar","7711922383");
+		Datos Persona2=new Datos("hola","prueba");
 		crear_archivo();
 		Persona.crear_informacion_usuario();
 		Persona2.crear_informacion_usuario();
